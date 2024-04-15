5b1e7b0328b690c2680b54372901dbf4b418abc1
lmnitzsche <logannitzsche1@gmail.com>
2024-04-15 18:59:22 -0500

    finishing last test case

--- CS325_P2/websiteScrapper.py
+++ CS325_P2/websiteScrapper.py
@@ -15,25 +15,28 @@ class ScrapeArticle:
         self.url = url
 
     def scrape_article(self):
-        # This first part is using the python program form module 1
-        # Takes in a url and then extracts the article text from the website
-        file = websiteScrap.BeautifulSoupScrapper()
-        file.requestURL(self.url)
-        article_text = file.extract()
-        
-        # This uses module 2 to write the newly processed article text to a new file inside the processed folder were its just the single string of the article text
-        filehandler = fileHandler.FileHandling(article_text,self.url)
-        filehandler.writeToProcessed()
+        # Checking if articles are scrapped correctly
+        try:
+            # This first part is using the python program form module 1
+            # Takes in a url and then extracts the article text from the website
+            file = websiteScrap.BeautifulSoupScrapper()
+            file.requestURL(self.url)
+            article_text = file.extract()
+            
+            # This uses module 2 to write the newly processed article text to a new file inside the processed folder were its just the single string of the article text
+            filehandler = fileHandler.FileHandling(article_text,self.url)
+            filehandler.writeToProcessed()
 
-        # This uses module 3 to send the single string containing the article text and sends it over to a LLM using the openai API
-        # This will return a new string containg a concised version of the article with a new title
-        concise = aiProcessor.LLMConciser(article_text)
-        concised_article_text = concise.generate_concise_description()
+            # This uses module 3 to send the single string containing the article text and sends it over to a LLM using the openai API
+            # This will return a new string containg a concised version of the article with a new title
+            concise = aiProcessor.LLMConciser(article_text)
+            concised_article_text = concise.generate_concise_description()
 
-        # This uses module 2 again to write the concised article to a new file inside the concised folder
-        filehandler.getArticle(concised_article_text)
-        filehandler.writeToConcised()
-
+            # This uses module 2 again to write the concised article to a new file inside the concised folder
+            filehandler.getArticle(concised_article_text)
+            filehandler.writeToConcised()
+        except Exception as e:
+            print(f"Error scraping article from {self.url}: {e}")
 #Test if the articles.text file was read in correctly
 try:
     # This is where you will put your article text file, the Data/raw/ is there because you should save your article text file inside the raw folder
