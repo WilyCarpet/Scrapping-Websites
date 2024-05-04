ef654901a296fb8e02ffec1b7fa7f31e8d3bab9f
WilyCarpet <126929634+WilyCarpet@users.noreply.github.com>
2024-05-03 20:46:48 -0500

    made webpage look better

--- webpage_creation/CreateHTML.py
+++ webpage_creation/CreateHTML.py
@@ -14,26 +14,43 @@ def txt_to_html(txt_file, html_file):
     with open(txt_file, 'r') as f:
         content = f.readlines()
 
-    # Create root element for HTML, try to remember the structure of a HTML file
+    # Create root element for HTML
     root = ET.Element("html")
 
-    # Create head and body elements, try to understand how subElements works
+    # Create head and body elements
+
+    #Content inside head element
     head = ET.SubElement(root, "head")
-    link = ET.SubElement(head,"link rel=\"stylesheet\" href=\"stylesheet.css\"")
+    ET.SubElement(head,"link rel=\"stylesheet\" href=\"stylesheet.css\"")
     title = ET.SubElement(head, "title")
     title.text = "My News Aggregation Site"
     body = ET.SubElement(root, "body")
 
+    #Content inside header element
+    header = ET.SubElement(body,"header")
+    ET.SubElement(header,"img src=\"Images/OPENAI symbol.webp\" alt=\"Open AI symbol\" class=\"header_img\"")
+    header_h1 = ET.SubElement(header,"h1 class=\"header_h1\"")
+    header_h1.text = "OpenAI Summarizes Articles"
+
+    #Content inside main element
+    main = ET.SubElement(body,"main")
+    main_dev = ET.SubElement(main,"dev class=\"main_center\"")
+
+    #Content inside footer element
+    footer = ET.SubElement(body,"footer")
+    p_footer = ET.SubElement(footer,"p")
+    p_footer.text = "\u00A9 Austin Luebbers"
+
     # Extract header and paragraph for each article
     for line in range(1,len(content),2):
 
-        header = content[line - 1].strip()
-        paragraph = "".join(content[line]).strip()
+        article_header = content[line - 1].strip().replace('"', '')
+        paragraph = "\t" + content[line].strip()
 
-        # Create header and paragraph elements in body
-        h2 = ET.SubElement(body, "h2")
-        h2.text = header
-        p = ET.SubElement(body, "p")
+        # Create header and paragraph elements in main
+        h2 = ET.SubElement(main_dev, "h2 class =\"main_h2\"")
+        h2.text = article_header
+        p = ET.SubElement(main_dev, "p class=\"main_p\"")
         p.text = paragraph
 
     # Write HTML tree to file
